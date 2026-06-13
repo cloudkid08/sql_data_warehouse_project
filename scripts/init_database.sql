dfbcd21aa4aa96e49d08e9de020ca022acf51359
Govardhan Balakrishnan <118262198+cloudkid08@users.noreply.github.com>
2026-06-13 13:04:30 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,33 @@
+-- ============================================================================ --
+				-- CREATING DATAWAREHOUSE AND SCHEMAS---
+-- ============================================================================ --
+/* i)First checking whether the Datawarehouse exists or not in the sql server database.
+   ii) IF not creating a new database called datawarehouse
+   iii) Creating three schemas for three layers bronze, silver, and gold.
+ */
 
+ -- Checking existing database
+Go
+If exists (select 1 from sys.databases where name ='Datawarehouse')
+Begin
+Alter database Datawarehouse set multi_user with rollback immediate;
+Drop database DataWarehouse;
+end;
+
+--Creating Database
+
+Create Database DataWarehouse;
+Go
+
+--- Using Database
+use DataWarehouse;
+Go
+
+--Creating Schemas
+
+create schema bronze;
+Go
+create schema silver;
+Go
+create schema gold;
+Go
