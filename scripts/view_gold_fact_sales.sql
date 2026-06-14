d38ee8cf5a03c5a04de0f5bbfc882da2d728f794
Govardhan Balakrishnan <118262198+cloudkid08@users.noreply.github.com>
2026-06-14 07:48:57 +0530

    Update view_gold_fact_sales.sql

--- scripts/view_gold_fact_sales.sql
+++ scripts/view_gold_fact_sales.sql
@@ -1,1 +1,17 @@
+--- creating facts table and joining with dimension tables ------
+create view gold.fact_sales as
 
+select sls_ord_num as order_number,
+pr.product_key,
+cu.customer_key,
+sls_order_dt as order_date,
+sls_ship_dt as shipping_date,
+sls_due_dt as due_date,
+sls_sales as sales,
+sls_quantity as quantity,
+sls_price as price
+from silver.crm_sales_details sd
+left join gold.dim_product as pr
+on sd.sls_prd_key=pr.product_number
+left join gold.dim_customers as cu
+on sd.sls_cust_id=cu.customer_id
